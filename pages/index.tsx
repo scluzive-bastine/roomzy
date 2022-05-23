3d1c048f0dd47528b37df9911fd36f360efac386
Sabastine <toshfuture@gmail.com>
2022-05-23 09:35:20 +0100

    House Categories component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,17 @@ const Home: NextPage = () => {
   const IMG_URL =
     'https://images.unsplash.com/photo-1558036117-15d82a90b9b1?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670'
 
+  const img1 =
+    'https://images.unsplash.com/photo-1611602132416-da2045990f76?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287'
+  const img2 =
+    'https://images.unsplash.com/photo-1614846384571-1e31322ed3a9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTIzfHxob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800'
+  const img3 =
+    'https://images.unsplash.com/photo-1563720223420-70e9835a9cb3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI1fHxob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800'
+  const img4 =
+    'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287'
   return (
     <main className="mx-auto max-w-screen-2xl px-4">
-      <div className="relative">
+      <div className="relative pb-10">
         <div className="relative h-[400px] w-full rounded-3xl md:h-[400px] md:w-full xl:h-[600px]">
           <Image
             src={IMG_URL}
@@ -92,6 +100,68 @@ const Home: NextPage = () => {
               <button className="rounded-lg bg-[#fa6932] p-3 text-lg text-white">
                 <FiSearch />
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto max-w-screen-xl pb-10">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+          <div className="group relative h-[500px] w-full rounded-2xl">
+            <Image
+              src={img1}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-2xl"
+            />
+            <div className="absolute hidden h-full w-full rounded-2xl bg-[#0000006f] transition duration-150 ease-in-out group-hover:flex">
+              <div className="absolute bottom-10 left-4">
+                <h1 className="text-xl font-semibold text-white">Outdoor</h1>
+                <p className="text-sm font-light text-white">131 Stays</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-5">
+            <div className="group relative h-[240px] w-full rounded-2xl">
+              <Image
+                src={img2}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-2xl"
+              />
+              <div className="absolute hidden h-full w-full rounded-2xl bg-[#0000006f] transition duration-150 ease-in-out group-hover:flex">
+                <div className="absolute bottom-10 left-4">
+                  <h1 className="text-xl font-semibold text-white">Outdoor</h1>
+                  <p className="text-sm font-light text-white">131 Stays</p>
+                </div>
+              </div>
+            </div>
+            <div className="group relative h-[240px] w-full rounded-2xl">
+              <Image
+                src={img3}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+              <div className="absolute hidden h-full w-full rounded-2xl bg-[#0000006f] transition duration-150 ease-in-out group-hover:flex">
+                <div className="absolute bottom-10 left-4">
+                  <h1 className="text-xl font-semibold text-white">Outdoor</h1>
+                  <p className="text-sm font-light text-white">131 Stays</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group relative h-[500px] w-full rounded-2xl">
+            <Image
+              src={img4}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-2xl"
+            />
+            <div className="absolute hidden h-full w-full rounded-2xl bg-[#0000006f] transition duration-150 ease-in-out group-hover:flex">
+              <div className="absolute bottom-10 left-4">
+                <h1 className="text-xl font-semibold text-white">Outdoor</h1>
+                <p className="text-sm font-light text-white">131 Stays</p>
+              </div>
             </div>
           </div>
         </div>
